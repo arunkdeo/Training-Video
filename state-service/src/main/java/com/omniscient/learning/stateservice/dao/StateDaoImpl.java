0a7204c4893b1a0ce8b2ccca61f8968cb4186c13
arunkdeo <78995211+arunkdeo@users.noreply.github.com>
2023-04-30 14:19:24 +0530

    Add files via upload

---
+++ state-service/src/main/java/com/omniscient/learning/stateservice/dao/StateDaoImpl.java
@@ -0,0 +1,73 @@
+package com.omniscient.learning.stateservice.dao;
+
+import com.omniscient.learning.stateservice.model.State;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class StateDaoImpl implements IStateDao {
+
+    private static final List<State> STATES = new ArrayList<>();
+    private static final Map<String, State> STATE_BY_CODE = new HashMap<>();
+    private static final Map<String, State> STATE_BY_NAME = new HashMap<>();
+
+    @PostConstruct
+    public void prepareStateData() throws IOException {
+        List<String> stateCsv = Files.readAllLines(Paths.get("src/main/resources/states.csv"));
+        stateCsv.remove(0);
+        stateCsv.forEach(state -> {
+            String[] sa = state.split(",");
+            State so = new State(sa[0], sa[1], sa[2], sa[3].split("\\|"));
+            STATES.add(so);
+            STATE_BY_CODE.put(so.getStateCode(), so);
+            STATE_BY_NAME.put(so.getStateName(), so);
+        });
+    }
+
+    @Override
+    public List<State> getStates() {
+        return new ArrayList<>(STATES);
+    }
+
+    @Override
+    public State getStateByCode(String stateCode) {
+        return STATE_BY_CODE.get(stateCode);
+    }
+
+    @Override
+    public List<String> getCitiesOfState(String stateName) {
+        return STATE_BY_NAME.get(stateName).getMajorCities();
+    }
+
+    @Override
+    public boolean createState(State state) {
+
+        return false;
+    }
+
+    @Override
+    public State updateState(State state) {
+        return null;
+    }
+
+    @Override
+    public State deleteState(State state) {
+        STATES.remove(state);
+        STATE_BY_NAME.remove(state.getStateName());
+        STATE_BY_CODE.remove(state.getStateCode());
+        return state;
+    }
+
+    @Override
+    public State getStateByName(String stateName) {
+        return STATE_BY_NAME.get(stateName);
+    }
+}
